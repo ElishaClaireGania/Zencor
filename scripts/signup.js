aaa137cea029a4cc58471c1b4b9c8848b383389e
airarosal <airamaetrosal14@gmail.com>
2026-03-21 00:32:07 +0800

    Update signup.js

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,36 +1,35 @@
 document.getElementById("signupForm").addEventListener("submit", function(e){
 
-e.preventDefault();
+  e.preventDefault();
 
-const data = {
+  const data = {
+    firstName: document.getElementById("firstName").value,
+    lastName: document.getElementById("lastName").value,
+    email: document.getElementById("email").value,
+    password: document.getElementById("password").value
+  };
 
-firstName: document.getElementById("firstName").value,
-lastName: document.getElementById("lastName").value,
-email: document.getElementById("email").value,
-password: document.getElementById("password").value
+  fetch("http://localhost:3000/signup", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+  .then(res => res.json())
+  .then(response => {
 
-};
+    if(response.message === "Account created successfully"){
 
-fetch("http://localhost:3000/signup",{
+      alert("Welcome to Zencor! 🎉");
 
-method:"POST",
+      localStorage.setItem("zencorUser", JSON.stringify({
+        email: data.email
+      }));
 
-headers:{
-"Content-Type":"application/json"
-},
+      window.location.href = "index.html";
+    }
 
-body:JSON.stringify(data)
-
-})
-
-.then(res => res.json())
-.then(data => {
-
-if(data.message === "Account created successfully"){
-alert("Account created successfully! Please login.");
-window.location.href = "login.html";
-}
-
-});
+  });
 
 });
